2617a4230b9f37900cc004acbc668c67066943ca
Jason Bodman <bodmand1@gmail.com>
2018-02-19 15:39:00 -0500

    add JSON functionality for restaurant menus

--- database_setup.py
+++ database_setup.py
@@ -9,13 +9,13 @@ Base = declarative_base()
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
-    
+
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
-    
+
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     course = Column(String(250))
@@ -23,6 +23,17 @@ class MenuItem(Base):
     price = Column(String(8))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+
+    @property
+    def serialize(self):
+
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -60,6 +60,12 @@ def deleteMenuItem(restaurant_id, menu_id):
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('deletemenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=deletedItem)
+
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems = [i.serialize for i in items])
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
